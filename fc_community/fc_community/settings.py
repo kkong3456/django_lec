57bb56373860c6923e09decba1f41246338d1f7d
kkong3456 <kkong3456@gmail.com>
2021-09-27 00:48:42 +0900

    pythonanywhere 배포완료

--- fc_community/fc_community/settings.py
+++ fc_community/fc_community/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8_1&rj14h4=f(xoouxuu8kxvyn6m=e%(5&7w6a&%4n=)38#x*^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kkong3456.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -122,9 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'static')
+# ]
+
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
